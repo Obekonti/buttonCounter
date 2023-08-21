f99e408a3ac79d371aeb9edd4209a3f5f50185a0
Obekonti <obekonti@icloud.com>
2023-08-21 10:49:43 +0600

    Создано приложение по подсчету нажатий на экран

--- buttonCounter/ViewController.swift
+++ buttonCounter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonRead: UILabel!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonRead.text = String("Значение счетчика: \(counter)")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonCounter(_ sender: Any) {
+        counter += 1
+        buttonRead.text = String("Значение счетчика: \(counter)")
+    }
+    
 }
 
